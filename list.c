70f6e36ee051aa274c350b144284cdd11c558931
Andy Edzo Nkily <edzo_nkily.andy@courrier.uqam.ca>
2022-12-20 11:12:45 -0500

    Ajouter l'implémentation de l'initialisation des listes

---
+++ list.c
@@ -0,0 +1,27 @@
+#include "list.h"
+
+struct recette *initialiser_recette(char *nom_recette){
+   struct recette *new_recette = (struct recette*) malloc(sizeof(struct recette));
+   new_recette->nom_recette = nom_recette;
+   new_recette->suivant = NULL;
+   return new_recette;
+}
+
+struct liste_recettes *initialiser_liste_recettes(){
+   struct liste_recettes *new_liste_recettes = (struct liste_recettes*) malloc(sizeof(struct liste_recettes));
+   new_liste_recettes->nb_recettes = 0;
+   new_liste_recettes->recettes = NULL;
+}
+
+struct categorie *initialiser_categorie(char *nom_categorie){
+   struct categorie *new_categorie = (struct categorie*) malloc(sizeof(struct categorie));
+   new_categorie->nom_categorie = nom_categorie;
+   new_categorie->suivant = NULL;
+   new_categorie->recettes = initialiser_liste_recettes();
+}
+
+struct liste_categories *initialiser_liste_categories(){
+   struct liste_categories *new_liste_categories = (struct liste_categories*) malloc(sizeof(struct liste_categories));
+   new_liste_categories->nb_categories = 0;
+   new_liste_categories->categories = NULL;
+}
