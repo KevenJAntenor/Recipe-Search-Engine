e5c26242a009f6a887d343309e6b9b899ae1deca
Messan, Ekoue Andy Scott <messan.ekoue_andy_scott@courrier.uqam.ca>
2022-12-21 20:26:13 -0500

    Ajout et implementation des fonctions de recherche

--- recherche.c
+++ recherche.c
@@ -1,15 +1,41 @@
 #include "recherche.h"
 
-void rechercher_par_categorie(){
-
+void rechercher_par_categorie(struct liste_categories *categories, char *critereDeRecherche){
+    struct categorie *temp = categories->categories ; int i = 0 ; bool recherche = true ;
+    while(i < categories->nb_categories && recherche ){
+        if(strstr(critereDeRecherche,temp->nom_categorie) != NULL) {
+            //METHODE PRINT_RECETTES_CATEGORIE ;
+            recherche = false ;
+        }
+        temp = temp->suivant ;
+        i++ ;
+    }
+    if(recherche) printf("\nCategorie inexistante.\n") ;
 }
 
-void rechercher_par_categorie_et_mot_cle(){
-
+void parcourir_recettes(struct liste_recettes *listeRecette, char *buffer, bool recherche){
+    struct recette *temp = listeRecette->recettes ; int i = 0;
+    while(i< listeRecette->nb_recettes && recherche){
+        if(strstr(buffer,temp->nom_recette) != NULL){
+            printf("%s.\n",temp->nom_recette) ;
+            recherche = false ;
+        }
+        temp = temp->suivant ; 
+        i++ ;
+    }
 }
 
-void rechercher(){
-
+void rechercher_par_categorie_et_mot_cle(struct liste_categories *categories, char *critereDeRecherche){
+    struct categorie *temp = categories->categories ; int i = 0 ; bool rechercheC = true, rechercheR = true ; char *buffer = strtok(critereDeRecherche," ");
+    while(i < categories->nb_categories && rechercheC ){
+        if(strstr(buffer,temp->nom_categorie) != NULL) {
+            buffer = strtok(NULL," ") ; rechercheC = false ;
+            parcourir_recettes(temp->recettes,buffer,&rechercheR) ;
+        }
+        temp = temp->suivant ;
+        i++ ;
+    }
+    if(rechercheC) printf("\nCategorie inexistante.\n") ; if(rechercheR) printf("\nAucun resultat trouve.\n") ;
 }
 
 char *enleve_espace_deb_fin(char *chaineDeCaractere){
@@ -34,18 +60,33 @@ char *enleve_espace_milieu(char *chaineDeCaractere){
     return chaineDeCaractere ;
 }
 
+int compter_nombre_espaces(char *chaineDeCaractere){
+    int i = 0 , nb = 0 ; 
+    while(i < strlen(chaineDeCaractere)){
+        if(chaineDeCaractere[i] == ' '){
+            nb++ ;
+        }
+    }
+    return nb ;
+}
+
 void traiter_entree_de_utilisateur(char *critereDeRecherche){
     char *critere ;
     strcpy(critere,enleve_espace_milieu(enleve_espace_deb_fin(critereDeRecherche))) ;
     for(int i = 0; i < strlen(critere); i++) critere[i] = tolower(critere[i]) ;
+    strcpy(critereDeRecherche,critere) ;
 }
+
 
 void lancer_recherche(struct liste_categories *categories){
     char critereDeRecherche[150] = "" ; 
     do{
-        print("Entrez votre critère de recherche(ou sinon 'q' pour quitter) :") ;
-        scanf("%s",&critereDeRecherche) ;
-        traiter_entree_de_utilisateur(critereDeRecherche) ;
-    }while(strcmp(critereDeRecherche,"q") == 0) ;
-    
+        printf("Entrez votre critère de recherche(ou sinon 'q' pour quitter) :") ;
+        scanf("%s",(char *)&critereDeRecherche) ;
+        traiter_entree_de_utilisateur((char *)&critereDeRecherche) ;
+        if(compter_nombre_espaces(critereDeRecherche) >= 0 && compter_nombre_espaces(critereDeRecherche) < 2){
+            if(compter_nombre_espaces(critereDeRecherche) == 0) rechercher_par_categorie(categories,critereDeRecherche) ;
+            else(rechercher_par_categorie_et_mot_cle(categories,critereDeRecherche)) ;
+        }else printf("\nRecherche Invalide.\n") ;
+    }while(strcmp(critereDeRecherche,"q") != 0) ;  
 }
--- recherche.h
+++ recherche.h
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 #include "list.h"
 
@@ -30,3 +31,32 @@ void traiter_entree_de_utilisateur(char *critereDeRecherche) ;
  * @param categories la liste des categories
 */
 void lancer_recherche(struct liste_categories *categories) ;
+
+/**
+ * Fait une recherche par categorie
+ * @param categories la liste des categories
+ * @param critereDeREcherche la categorie a chercher 
+*/
+void rechercher_par_categorie(struct liste_categories *categories, char *critereDeRecherche) ;
+
+/**
+ * Fait une recherche par categorie avec un mot cle
+ * @param categories la liste des categories
+ * @param critereDeREcherche la categorie a chercher et son mot cle
+*/
+void rechercher_par_categorie_et_mot_cle(struct liste_categories *categories, char *critereDeRecherche) ;
+
+/**
+ * Permet de parcourir les recettes
+ * @param listeREcette la liste de recettes
+ * @param buffer le mot cle
+ * @param recherche mot recherche pas trouve ?
+*/
+void parcourir_recettes(struct liste_recettes *listeRecette, char *buffer, bool recherche) ;
+
+/**
+ * Compte le nombred'espaces et le retourne
+ * @param chaineDeCaractere la chaine de caracteres
+ * @return nombre d'espaces
+*/
+int compter_nombre_espaces(char *chaineDeCaractere) ;
